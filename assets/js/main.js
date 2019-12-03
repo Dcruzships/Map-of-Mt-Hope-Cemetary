25c3a703615dc0cceead21f59e871beb15c439fb
Brandooo! <bvd5889@rit.edu>
2019-12-02 21:49:48 -0500

    Styles and Data

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,154 @@
 let map;
 let infoWindow;
 
-function initMap()
-{
-  let mapOptions =
-  {
-    center: {lat:43.128658, lng:-77.620677},
-    minZoom: 10,
-    maxZoom: 50,
-    zoom:18,
+function initMap() {
+  let mapOptions = {
+    center: {
+      lat: 43.128658,
+      lng: -77.620677
+    },
+    minZoom: 15,
+    maxZoom: 80,
+    zoom: 18,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    restriction:
-    {
-      latLngBounds:
+    styles: [{
+        elementType: 'geometry',
+        stylers: [{
+          color: '#242f3e'
+        }]
+      },
       {
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#242f3e'
+        }]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#746855'
+        }]
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [{
+          "visibility": "off"
+        }]
+      },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#d59563'
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#020502"
+          },
+          {
+            "lightness": 21
+          },
+          {
+            "visibility": "on"
+          }
+        ]
+      },
+
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#746855'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#1f2835'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#f3d19c'
+        }]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+          },
+          {
+            "lightness": 16
+          },
+          {
+            "visibility": "simplified"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "visibility": "on"
+          },
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "on"
+          },
+          {
+            "color": "#422111"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#011424"
+          },
+          {
+            "lightness": 17
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+            "color": "#422111"
+          },
+          {
+            "weight": "1.71"
+          },
+          {
+            "lightness": "8"
+          }
+        ]
+      },
+    ],
+    restriction: {
+      latLngBounds: {
         east: -77.6068,
         north: 43.1374,
         south: 43.1231,
@@ -32,50 +167,66 @@ function initMap()
   );
 
   map = new google.maps.Map(document.querySelector("#map"), mapOptions);
+  google.maps.event.addListener(map, 'click', function(e) {
+    // Close old InfoWindow if it's up
+    if (infoWindow) {
+      infoWindow.close();
+    }
+  });
 
-  for (let i = 0; i < trash.length; i++)
-  {
-    let lat = trash[i].latitude;
-    let long = trash[i].longitude;
-    let name = trash[i].title;
-    let cat = trash[i].category;
-    let pic = trash[i].url;
-
-    addMarker(lat, long, name, pic, cat);
-  }
+  addMarkers(dead);
+  addMarkers(symbols);
 }
 
-function makeInfoWindow(position, msg)
-{
+function makeInfoWindow(position, title, pic, body) {
   // Close old InfoWindow if it's up
-  if(infoWindow) {infoWindow.close();}
+  if (infoWindow) {
+    infoWindow.close();
+  }
 
   // Make a new InfoWindow
   infoWindow = new google.maps.InfoWindow({
     map: map,
     position: position,
-    content: "<b>" + msg + "</b>"
+    content: `<b><u>${title}</u></b><br><br>${pic}<br><br>${body}`
   });
 }
 
-function addMarker(latitude, longitude, title, url, category)
-{
-  let position = {lat: latitude, lng: longitude};
-  let marker = new google.maps.Marker({position: position, map: map, url: url});
-  if(category == "dead")
-  {
+function makeMarker(id, name, category, latitude, longitude, body, pic) {
+  let position = {
+    lat: latitude,
+    lng: longitude
+  };
+  let marker = new google.maps.Marker({
+    position: position,
+    map: map
+  });
+  if (category == "dead") {
     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/purple-dot.png')
   }
-  else
-  {
+  if (category == "symbology") {
     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
   }
-  marker.setTitle(title);
+  marker.setTitle(name);
 
   // Add a listener for the click event
-  google.maps.event.addListener(marker, 'click', function(e)
-  {
+  google.maps.event.addListener(marker, 'click', function(e) {
     // window.open(this.url);
-    makeInfoWindow(this.position, this.title);
+    makeInfoWindow(this.position, name, pic, body);
   });
+}
+
+function addMarkers(set) {
+  for (let i = 0; i < set.length; i++) {
+    let id = set[i].id;
+    let name = set[i].name;
+    let cat = set[i].category;
+    let lat = set[i].latitude;
+    let long = set[i].longitude;
+    let body = set[i].body;
+    let pic = `<img src='assets/img/${cat}/${id}.png'>`;
+    console.log(body);
+
+    makeMarker(id, name, cat, lat, long, body, pic);
+  }
 }
--- assets/js/trash-data.js
+++ assets/js/trash-data.js
@@ -1,55 +1,414 @@
-const trash = [
+const dead =
+[
   {
-    latitude:43.130,
-    longitude:-77.617,
-    category: "trash",
-    title:"<p><a href='https://www.youtube.com/watch?v=a49lzecUBq0' target='_blank'>a plastic bag</a><br><br><img class='markerImage' src='assets/img/bag.png'></p>",
-    url:"https://i.imgur.com/70ZXr7d.jpg"
-  },
-
-  {
-    latitude:43.131,
-    longitude:-77.613,
-    category: "trash",
-    title:"<p> love <br><br><img class='markerImagePort' src='assets/img/love.png'></p>",
-    url:"https://i.imgur.com/baWODUH.jpg"
-  },
-
-  {
-    latitude:43.128,
-    longitude:-77.61609,
-    category: "trash",
-    title:"<p> chicken <br><br><img class='markerImage' src='https://i.imgur.com/iF5dKQi.jpg'></p>",
-    url:"https://i.imgur.com/iF5dKQi.jpg"
-  },
-
-  {
-    latitude:43.128,
-    longitude:-77.616,
-    category: "trash",
-    title:"<p> angel <br><br><img class='markerImage' src='https://i.imgur.com/7uLZUpa.jpg'></p>",
-    url:"https://i.imgur.com/7uLZUpa.jpg"
-  },
-
-  {
-    latitude:43.125,
-    longitude:-77.618,
-    category: "trash",
-    title:"<p> frame <br><br><img class='markerImage' src='https://i.imgur.com/womKw2A.jpg'></p>",
+    id: "dead01",
+    name: "Dollie",
+    category: "dead",
+    latitude: 43.12986111,
+    longitude: -77.61604444,
+    body: "Missing head, stone corroding from people stepping over her. No date.",
     url:"https://i.imgur.com/womKw2A.jpg"
   },
 
   {
-    latitude: 43.13004,
-    longitude: -77.61894,
+    id: "dead02",
+    name: "Mau*e Rinker: 18** - 1882",
     category: "dead",
-    title: "<p> dead 1 <br><br><img class='markerImage' src='assets/img/dog1.png'></p>"
+    latitude: 43.13003889,
+    longitude: -77.61894167,
+    body: "Dog fading to white. Like snow on Mount Rushmore, a forever season, a forever white space of love and degredation.",
+    url:"https://i.imgur.com/womKw2A.jpg"
   },
 
   {
-    latitude: 43.13015,
-    longitude: -77.61876,
+    id: "dead03",
+    name: "Louise Serth: 1897 - 1898",
     category: "dead",
-    title: "<p> dead 2 <br><br><img class='markerImagePort' src='assets/img/dog2.png'></p>"
+    latitude: 43.12916667,
+    longitude: -77.62225278,
+    body: "How does a puppy die in one year? Was she run over by a car, or thrown into a woodchipper, or did she smoke too much and get breast cancer, or was she put down due to her energy, or did she just die? Can dogs just die?",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead04",
+    name: "ORVILLE: 1893/7 - 189*",
+    category: "dead",
+    latitude: 43.12927500,
+    longitude: -77.62219167,
+    body: "Illegible text under the date. First word ends with d, last has a Y and an S. 'Loved 3 Years?' 'Lived & Yells?' 'Liked 3 Yips?'",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead05",
+    name: "undefined1",
+    category: "dead",
+    latitude: 43.12585833,
+    longitude: -77.62672500,
+    body: "Weathered away into bliss. Or darkness. Or love. Or loneliness. Or a new dawn. Or a beach with palm trees. Or a castle on clouds. Or a new network of souls. Or ",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead06",
+    name: "Mabel J.VanSi*e: 1911 - 1921",
+    category: "dead",
+    latitude: 43.12618889,
+    longitude: -77.62673889,
+    body: "10 years, 4 names. Was a darling.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead07",
+    name: "SON Carla Smargz: 1917 - 1921",
+    category: "dead",
+    latitude: 43.12606944,
+    longitude: -77.62672500,
+    body: "Maybe Carla had a son, maybe she was a son. Maybe SON is an acronym- 'Signaling Optimizing Nugget.' Or something else. Text underneath illegible, looks like 'asleep.'",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead08",
+    name: "undefined2",
+    category: "dead",
+    latitude: 43.12613333,
+    longitude: -77.62677778,
+    body: "Looks like the animal is melting into the ground. Bird poop smothered. Cracks underneath like the puppy is too heavy for the rock. One kick, one shove, one punch away from nothingness. Not a memory or symbol of anything, like 'undefined2' never existed.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead09",
+    name: "undefined3",
+    category: "dead",
+    latitude: 43.12614444,
+    longitude: -77.62685278,
+    body: "Complete erosion, going back. Into the earth, or to the hands of God. Seeking something again, soon. But not now, for now the rock still stands for a little while more. Cracks on the top and bottom appear intentional, like somebody took a hammer to the base out of anger, love, jealousy, regret, drunkeness, whatever.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead10",
+    name: "James Leonard: Apr. 9-30, 1920",
+    latitude: 43.12635000,
+    longitude: -77.62677778,
+    category: "dead",
+    body: "3 weeks. 21 days. 504 hours. 30,240 minutes. Son of ChaS. & Bessie Sloan, good job.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead11",
+    name: "undefined4",
+    category: "dead",
+    latitude: 43.12610556,
+    longitude: -77.62645000,
+    body: "We're not photographers. Emily is, the rest of us aren't. Text is too blurry to make out, like those graves fading away, like a printer running out of ink, like a memory that you can't exactly recall. Almost there, is that good enough?",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead12",
+    name: "Frank E/L. Pierson: June 14 1918 - May 6 1915",
+    category: "dead",
+    latitude: 43.12628611,
+    longitude: -77.62606111,
+    body: "Dog is white, snow pup. Stone melting. Dripping. If black is nothing and white is everything, why do these tombstones prefer a way to go? Maybe they go backwards.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead13",
+    name: "PITCHER",
+    category: "dead",
+    latitude: 43.12631944,
+    longitude: -77.62634167,
+    body: "Like a tree. Or a scyscraper. Or a lighthouse. Or a dildo. Or a crashed plane. Or a Juul. Or a giraffe. Or a buoy at sea, a marker of safety, a beacon of hope.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead14",
+    name: "SON George A. Corson 3rd DIED Jan. 20 1919 Age 18 Months",
+    category: "dead",
+    latitude: 43.12645278,
+    longitude: -77.62642778,
+    body: "There's something about the burial of young pets that says something about people. Instead of taxidermy or cremation or tossing in the ocean, they spend the money, they bury the son. Sometimes anyways.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead15",
+    name: "DAVIE",
+    category: "dead",
+    latitude: 43.12826111,
+    longitude: -77.62264167,
+    body: "A pyramid for a pharaoh/mummy/prince/princess/god/dog",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead16",
+    name: "McCORMICK JAMES DIED NOV.11.1911 JANE his wife DIED DEC.13.1910 ANNIE E- DIED SEPT.15.1918 EMMA ALDRICH DIED MAY 31,1923",
+    category: "dead",
+    latitude: 43.12827778,
+    longitude: -77.62345000,
+    category: "dead",
+    body: "tiny monkeys, sown together in a heart.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead17",
+    name: "undefined5",
+    category: "dead",
+    latitude: 43.12953889,
+    longitude: -77.62386389,
+    body: "Stains of moss, snow surrounding puppy like quicksand. Gone.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead18",
+    name: "undefined6",
+    category: "dead",
+    latitude: 43.12964167,
+    longitude: -77.62295556,
+    body: "White. Light. Everything.",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "dead19",
+    name: "LOUISE SERTH 1897 - 1898",
+    category: "dead",
+    latitude: 43.12916667,
+    longitude: -77.62225278,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
   }
+];
+
+const symbols =
+[
+  {
+    id: "symbol1",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13438056,
+    longitude: -77.61526389,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol2",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13352222,
+    longitude: -77.61513611,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol3",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13340833,
+    longitude: -77.61496667,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol4",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13141389,
+    longitude: -77.61521111,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol5",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12985278,
+    longitude: -77.61668333,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol6",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12988333,
+    longitude: -77.61881389,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol7",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12932222,
+    longitude: -77.61921667,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol8",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12938611,
+    longitude: -77.61941667,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol9",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12928333,
+    longitude: -77.61964444,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol10",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12940278,
+    longitude: -77.61976667,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol11",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12940278,
+    longitude: -77.61977500,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol12",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.12923333,
+    longitude: -77.61985833,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol13",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13026944,
+    longitude: -77.61557778,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol14",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13077222,
+    longitude: -77.61511111,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol15",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13245833,
+    longitude: -77.61435000,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  },
+
+  {
+    id: "symbol16",
+    name: "OES Star",
+    category: "symbology",
+    latitude: 43.13238889,
+    longitude: -77.61452500,
+    body: "Font: Roboto Slab",
+    url:"https://i.imgur.com/womKw2A.jpg"
+  }
+];
+
+const trash =
+[
+  // {
+  //   latitude:43.130,
+  //   longitude:-77.617,
+  //   category: "trash",
+  //   title:"<p><a href='https://www.youtube.com/watch?v=a49lzecUBq0' target='_blank'>a plastic bag</a><br><br><img class='markerImage' src='assets/img/bag.png'></p>",
+  //   url:"https://i.imgur.com/70ZXr7d.jpg"
+  // },
+  //
+  // {
+  //   latitude:43.131,
+  //   longitude:-77.613,
+  //   category: "trash",
+  //   title:"<p> love <br><br><img class='markerImagePort' src='assets/img/love.png'></p>",
+  //   url:"https://i.imgur.com/baWODUH.jpg"
+  // },
+  //
+  // {
+  //   latitude:43.128,
+  //   longitude:-77.61609,
+  //   category: "trash",
+  //   title:"<p> chicken <br><br><img class='markerImage' src='https://i.imgur.com/iF5dKQi.jpg'></p>",
+  //   url:"https://i.imgur.com/iF5dKQi.jpg"
+  // },
+  //
+  // {
+  //   latitude:43.128,
+  //   longitude:-77.616,
+  //   category: "trash",
+  //   title:"<p> angel <br><br><img class='markerImage' src='https://i.imgur.com/7uLZUpa.jpg'></p>",
+  //   url:"https://i.imgur.com/7uLZUpa.jpg"
+  // },
+  //
+  // {
+  //   latitude:43.125,
+  //   longitude:-77.618,
+  //   category: "trash",
+  //   title:"<p> frame <br><br><img class='markerImage' src='https://i.imgur.com/womKw2A.jpg'></p>",
+  //   url:"https://i.imgur.com/womKw2A.jpg"
+  // },
+  //
+  // {
+  //   latitude: 43.13004,
+  //   longitude: -77.61894,
+  //   category: "dead",
+  //   title: "<p> dead 1 <br><br><img class='markerImage' src='assets/img/dog1.png'></p>"
+  // },
+  //
+  // {
+  //   latitude: 43.13015,
+  //   longitude: -77.61876,
+  //   category: "dead",
+  //   title: "<p> dead 2 <br><br><img class='markerImagePort' src='assets/img/dog2.png'></p>"
+  // }
+
 ];
